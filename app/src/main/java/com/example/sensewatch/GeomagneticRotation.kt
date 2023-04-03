80099f1f444fca9a94b0d6b30fe52d63d99b378e
Mohammad Atif Quamar <atif20523@iiitd.ac.in>
2023-04-03 22:50:05 +0530

    smoothened, but not much effect

--- app/src/main/java/com/example/sensewatch/GeomagneticRotation.kt
+++ app/src/main/java/com/example/sensewatch/GeomagneticRotation.kt
@@ -9,53 +9,54 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
-class GeomagneticRotation : AppCompatActivity() {
+class GeomagneticRotation : AppCompatActivity(), SensorEventListener  {
 
-    private lateinit var sensorManager: SensorManager
-    private lateinit var geomagneticSensor: Sensor
     private lateinit var tvXCoordinate: TextView
     private lateinit var tvYCoordinate: TextView
     private lateinit var tvZCoordinate: TextView
+    private lateinit var sensorManager: SensorManager
     private var rotationVectorSensor: Sensor? = null
-    private val filterFactor = 0.2f // Filter factor
+
+    private val filterFactor = 0.9f // Filter factor
     private var smoothedRotationVector = FloatArray(4)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_geomagnetic_rotation)
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        geomagneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
+        rotationVectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
 
         tvXCoordinate = findViewById(R.id.tv_X_val)
         tvYCoordinate = findViewById(R.id.tv_Y_val)
         tvZCoordinate = findViewById(R.id.tv_Z_val)
     }
 
-    private val sensorEventListener = object : SensorEventListener {
-        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-            // Do nothing
-        }
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_ROTATION_VECTOR) {
+            // Apply low-pass filter to smooth the sensor data
+            smoothedRotationVector[0] += filterFactor * (event.values[0] - smoothedRotationVector[0])
+            smoothedRotationVector[1] += filterFactor * (event.values[1] - smoothedRotationVector[1])
+            smoothedRotationVector[2] += filterFactor * (event.values[2] - smoothedRotationVector[2])
+            smoothedRotationVector[3] += filterFactor * (event.values[3] - smoothedRotationVector[3])
 
-        override fun onSensorChanged(event: SensorEvent?) {
-            if (event?.sensor?.type == Sensor.TYPE_ROTATION_VECTOR) {
-                val rotationMatrix = FloatArray(9)
-                SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values)
-                val orientation = FloatArray(3)
-                SensorManager.getOrientation(rotationMatrix, orientation)
-                val x = orientation[0] * 180 / Math.PI
-                val y = orientation[1] * 180 / Math.PI
-                val z = orientation[2] * 180 / Math.PI
-                tvXCoordinate.text = "$x"
-                tvYCoordinate.text = "$y"
-                tvZCoordinate.text = "$z"
-
-            }
+            // Update the TextViews with the smoothed sensor data
+            tvXCoordinate.text = String.format("%.9f", smoothedRotationVector[0])
+            tvYCoordinate.text = String.format("%.9f", smoothedRotationVector[1])
+            tvZCoordinate.text = String.format("%.9f", smoothedRotationVector[2])
         }
     }
 
-
     override fun onResume() {
         super.onResume()
-        sensorManager.registerListener(sensorEventListener, geomagneticSensor, SensorManager.SENSOR_DELAY_NORMAL)
+        rotationVectorSensor?.let {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // No-op
     }
 }
